498667f3377772c3be3ac0e1a8b04e855180ed2d
Zeina Kittaneh <zeinakittaneh@gmail.com>
2019-03-31 11:56:47 -0400

    fix names of locations

--- js/montreal-special-places.js
+++ js/montreal-special-places.js
@@ -1,62 +1,52 @@
 var montrealAttractions = [
   {
-    title:"Mont Royal",
-    lng:"45.5071",
-    lat:"-73.5874",
+    title:"Mount Royal",
+    location:{lat: 45.5071, lng: -73.5874},
     type:"hill"
   },
   {
-    title:"Vieux-Montreal (Old Montreal)",
-    lng:"45.5075",
-    lat:"-73.5544",
+    title:"Old Montreal",
+    location:{lat: 45.5075, lng: -73.5544},
     type:"historic buildings"
   },
   {
-    title:"Jardin Botanique (Botanical Garden)",
-    lng:"45.5600",
-    lat:"-73.5630",
+    title:"Botanical Garden",
+    location:{lat: 45.5600, lng: -73.5630},
     type:"park"
   },
   {
-    title:"Notre-Dame Basilica",
-    lng:"45.5045",
-    lat:"-73.5561",
+    title:"Notre-Dame Basilica (Montreal)",
+    location:{lat: 45.5045, lng: -73.5561},
     type:"church"
   },
   {
-    title:"Lachine Canal National Historic Site",
-    lng:"45.4666648",
-    lat:"-73.589330976",
+    title:"Lachine Canal",
+    location:{lat: 45.4666648, lng: -73.589330976},
     type:"canal national historic site"
   },
   {
-    title:"Parc Jean Drapeau",
-    lng:"45.5171",
-    lat:"-73.5336",
+    title:"Parc Jean-Drapeau",
+    location:{lat: 45.5171, lng: -73.5336},
     type:"park"
   },
   {
-    title:"Musée des Beaux Arts (Fine Arts Museum)",
-    lng:"45.4985",
-    lat:"-73.5794",
+    title:"Montreal Museum of Fine Arts",
+    location:{lat: 45.4985, lng: -73.5794},
     type:"museum"
   },
   {
-    title:"Pointe-à-Callière",
-    lng:"45.5027",
-    lat:"-73.5542",
+    title:"Pointe-à-Callière Museum",
+    location:{lat: 45.5027, lng: -73.5542},
     type:"museum"
   },
   {
     title:"Place des Arts",
-    lng:"45.5083",
-    lat:"-73.5664",
+    location:{lat: 45.5083, lng: -73.5664},
     type:"museum"
   },
   {
-    title:"Atwater and Jean Talon Markets",
-    lng:"45.5365",
-    lat:"-73.6147",
+    title:"Atwater Market",
+    location:{lat: 45.5365, lng: -73.6147},
     type:"market"
   }
 ];
