1d76bdb9793a41ccb1a3561e456d60a9d99d0dc7
Zeina Kittaneh <zeinakittaneh@gmail.com>
2019-03-30 22:52:16 -0400

    adding tourist attractions

---
+++ js/montreal-special-places.js
@@ -0,0 +1,62 @@
+var montrealAttractions = [
+  {
+    title:"Mont Royal",
+    lng:"45.5071",
+    lat:"-73.5874",
+    type:"hill"
+  },
+  {
+    title:"Vieux-Montreal (Old Montreal)",
+    lng:"45.5075",
+    lat:"-73.5544",
+    type:"historic buildings"
+  },
+  {
+    title:"Jardin Botanique (Botanical Garden)",
+    lng:"45.5600",
+    lat:"-73.5630",
+    type:"park"
+  },
+  {
+    title:"Notre-Dame Basilica",
+    lng:"45.5045",
+    lat:"-73.5561",
+    type:"church"
+  },
+  {
+    title:"Lachine Canal National Historic Site",
+    lng:"45.4666648",
+    lat:"-73.589330976",
+    type:"canal national historic site"
+  },
+  {
+    title:"Parc Jean Drapeau",
+    lng:"45.5171",
+    lat:"-73.5336",
+    type:"park"
+  },
+  {
+    title:"Musée des Beaux Arts (Fine Arts Museum)",
+    lng:"45.4985",
+    lat:"-73.5794",
+    type:"museum"
+  },
+  {
+    title:"Pointe-à-Callière",
+    lng:"45.5027",
+    lat:"-73.5542",
+    type:"museum"
+  },
+  {
+    title:"Place des Arts",
+    lng:"45.5083",
+    lat:"-73.5664",
+    type:"museum"
+  },
+  {
+    title:"Atwater and Jean Talon Markets",
+    lng:"45.5365",
+    lat:"-73.6147",
+    type:"market"
+  }
+];
